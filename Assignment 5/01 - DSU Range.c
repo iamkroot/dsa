c125ceb91ee158c15f0d04d2d620b49981d233b2
iamkroot <kroot.patel@gmail.com>
2019-02-18 12:22:41 +0530

    5.01: Add comments and clean up code

--- Assignment 5/01 - DSU Range.c
+++ Assignment 5/01 - DSU Range.c
@@ -3,7 +3,9 @@
 void run() {
     int n;
     scanf("%d\n", &n);
-    int ranges[MAX_NUM][3] = {}, root[MAX_NUM], include[MAX_NUM] = {0};
+    int ranges[MAX_NUM][3] = {},  // stores (l_index, r_index, team_num) for each range
+        root[MAX_NUM],
+        include[MAX_NUM] = {0};  // skip over the indices not in any range
     for (int i = 0; i < MAX_NUM; i++) {
         root[i] = i;
     }
@@ -11,37 +13,36 @@ void run() {
         int a, b;
         scanf("%d %d", &a, &b);
         a--;
-        b--;
+        b--; // 0-based indexing ftw
         ranges[i][0] = a;
         ranges[i][1] = b;
+
+        // update roots for all indices after a
         for (int j = a; j < MAX_NUM; j++) {
-            if (j <= b) {
+            if (j <= b) {  // inside the new range that has been entered
                 include[j] = 1;
                 root[j] = root[a];
-            } else if (root[j] >= a && root[j] <= b) {
+            } else if (a <= root[j] && root[j] <= b) {  // for those indices whose roots were inside the new range -> update roots to root[a]
                 root[j] = root[a];
             }
         }
     }
-    int team = 0, teams[MAX_NUM] = {0}, flag = 0;
+    int team = 0, not_possible = 1;
     for (int i = 0; i < MAX_NUM; i++) {
         if (!include[i])
             continue;
-        teams[i] = team;
-        if (i < MAX_NUM - 1 && root[i] != root[i + 1]) {
-            team = !team;
-        }
         for (int j = 0; j < n; j++) {
-            if (ranges[j][0] <= i && ranges[j][1] >= i) {
-                if (!teams[i] && !flag)
-                    flag++;
-                else if (flag == 1 && teams[i])
-                    flag = 2;
-                ranges[j][2] = teams[i];
+            if (ranges[j][0] <= i && i <= ranges[j][1]) {  // assign team to all ranges that contain the current index
+                if (team)
+                    not_possible = 0;  // both teams have been given at least one range
+                ranges[j][2] = team;
             }
         }
+        if (i < MAX_NUM - 1 && root[i] != root[i + 1]) {
+            team = !team;  // if the next root is different, change team. Basically, alternate the teams for disconnected ranges.
+        }
     }
-    if (flag < 2) {
+    if (not_possible) {  // if all ranges have same team
         printf("-1\n");
         return;
     }
